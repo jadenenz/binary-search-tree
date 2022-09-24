4800defd97c9d9000778ede7db5dc1d558fa22f5
jadenenz <jenzbr@gmail.com>
2022-09-23 20:49:11 -0500

    wrote the rest of the methods and the driver function

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,3 +1,5 @@
+import { toHaveAttribute } from "@testing-library/jest-dom/dist/matchers"
+
 export class Node {
   constructor(value) {
     this.value = value
@@ -7,14 +9,13 @@ export class Node {
 }
 
 export class Tree {
-  root = null
-  arr
   constructor(arr) {
     this.arr = arr
+    this.root = this.buildTree(this.arr)
   }
 
   getRoot() {
-    return Tree.root
+    return this.root
   }
 
   buildTree(arr) {
@@ -41,18 +42,18 @@ export class Tree {
     }
 
     const result = sortedArrayToBST(sortedArr, 0, arrayLength - 1)
-    Tree.root = result
+    this.root = result
     return result
   }
 
   insert(value) {
     const newNode = new Node(value)
-    if (Tree.root == null) {
-      Tree.root = newNode
+    if (this.root == null) {
+      this.root = newNode
       return
     }
     let prev = null
-    let temp = Tree.root
+    let temp = this.root
     while (temp !== null) {
       if (temp.value > newNode.value) {
         prev = temp
@@ -69,9 +70,180 @@ export class Tree {
     }
   }
 
-  delete(value) {
-    // https://www.geeksforgeeks.org/binary-search-tree-set-2-delete/?ref=lbp
+  deleteNode(value, root = this.root) {
+    if (root == null) {
+      return root
+    }
+
+    if (root.value > value) {
+      root.left = this.deleteNode(value, root.left)
+    } else if (root.value < value) {
+      root.right = this.deleteNode(value, root.right)
+    } else {
+      if (root.left == null) {
+        return root.right
+      } else if (root.right == null) {
+        return root.left
+      }
+      root.value = minValue(root)
+      root.right = this.deleteNode(root.right, root.value)
+    }
+
+    return root
   }
+
+  find(value, root = this.root) {
+    if (root == null) {
+      return root
+    }
+
+    if (root.value > value) {
+      return this.find(value, root.left)
+    } else if (root.value < value) {
+      return this.find(value, root.right)
+    } else {
+      return root
+    }
+  }
+
+  //Calls callback function on each node in the tree in level order, otherwise returns array
+  //of values in level order
+  levelOrder(callback) {
+    let queue = []
+    let nodeArray = []
+    queue.push(this.root)
+    while (queue.length !== 0) {
+      let tempNode = queue.shift()
+      if (arguments.length === 0) {
+        nodeArray.push(tempNode.value)
+      } else {
+        callback(tempNode)
+      }
+
+      //enqueue left child
+      if (tempNode.left != null) {
+        queue.push(tempNode.left)
+      }
+
+      //enqueue right child
+      if (tempNode.right != null) {
+        queue.push(tempNode.right)
+      }
+    }
+    if (arguments.length === 0) {
+      return nodeArray
+    }
+  }
+
+  //Calls callback function on each node in the tree in post order, otherwise returns array
+  //of values in post order
+  postOrder(callback, node = this.root, nodeArray = []) {
+    if (node === null) return
+
+    this.postOrder(callback, node.left, nodeArray)
+    this.postOrder(callback, node.right, nodeArray)
+    callback ? callback(node) : nodeArray.push(node.value)
+
+    if (nodeArray.length > 0) return nodeArray
+  }
+
+  //Calls callback function on each node in the tree in in order, otherwise returns array
+  //of values in in order
+  inOrder(callback, node = this.root, nodeArray = []) {
+    if (node === null) return
+
+    this.inOrder(callback, node.left, nodeArray)
+    callback ? callback(node) : nodeArray.push(node.value)
+    this.inOrder(callback, node.right, nodeArray)
+
+    if (nodeArray.length > 0) return nodeArray
+  }
+
+  //Calls callback function on each node in the tree in pre order, otherwise returns array
+  //of values pre in order
+  preOrder(callback, node = this.root, nodeArray = []) {
+    if (node === null) return
+
+    callback ? callback(node) : nodeArray.push(node.value)
+    this.preOrder(callback, node.left, nodeArray)
+    this.preOrder(callback, node.right, nodeArray)
+
+    if (nodeArray.length > 0) return nodeArray
+  }
+
+  depth(node = this.root) {
+    if (node === null) {
+      return -1
+    } else {
+      let lDepth = this.depth(node.left)
+      let rDepth = this.depth(node.right)
+
+      //use the larger one
+      if (lDepth > rDepth) {
+        return lDepth + 1
+      } else {
+        return rDepth + 1
+      }
+    }
+  }
+
+  height(node = this.root) {
+    if (node === null) {
+      return -1
+    } else {
+      let lHeight = this.height(node.left)
+      let rHeight = this.height(node.right)
+
+      console.log("lHeight: ", lHeight, "node value: ", node.value)
+      return Math.max(lHeight, rHeight) + 1
+    }
+  }
+
+  isBalanced(node = this.root) {
+    if (node === null) return
+
+    const heightDifference = Math.abs(
+      this.height(node.left) - this.height(node.right)
+    )
+
+    console.log("heightDiff : ", heightDifference)
+
+    // console.log("left abs: ", Math.abs(this.height(left)))
+    // console.log("right abs: ", Math.abs(this.height(right)))
+
+    if (heightDifference > 1) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  rebalance() {
+    const inOrderArray = this.inOrder()
+    console.log("inOrder: ", inOrderArray)
+    this.buildTree(inOrderArray)
+  }
+
+  prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
+    }
+    console.log(
+      `${prefix}${isLeft ? "└── " : "┌── "}${node.value} ${this.height(node)}`
+    )
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
+    }
+  }
+}
+
+function minValue(root) {
+  let min = root.value
+  while (root != null) {
+    min = root.value
+    root = root.left
+  }
+  return min
 }
 
 export const removeDuplicates = (arr) => {
@@ -84,7 +256,6 @@ export const removeDuplicates = (arr) => {
   return unique
 }
 
-//Figure out why this isn't working -- format?
 //Function received from TOP assignment that prints out the tree in a structured format
 export const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node.right !== null) {
--- src/binarySearchTree.test.js
+++ src/binarySearchTree.test.js
@@ -1,10 +1,94 @@
-import { Tree, removeDuplicates, prettyPrint } from "./binarySearchTree"
+import { Tree, prettyPrint } from "./binarySearchTree"
 
-test("log node", () => {
-  const test = new Tree()
-  const testArr = [1, 2, 3, 4, 5, 4, 2]
-  test.buildTree(testArr)
+// test("insert method works", () => {
+//   const test = new Tree()
+//   const testArr = [1, 2, 3, 4, 5, 4, 2]
+//   test.buildTree(testArr)
+//   // prettyPrint(test.getRoot())
+//   test.insert(12)
+//   // prettyPrint(test.getRoot())
+// })
+
+test("delete method works", () => {
+  const testArr = [1, 2, 3, 4, 5]
+  const test = new Tree(testArr)
+  // prettyPrint(test.getRoot())
+  test.deleteNode(4)
+  // console.log("-------------------------")
+  // prettyPrint(test.getRoot())
+})
+
+test("find method works", () => {
+  const testArray = [50, 24, 3001, 62, 99, 1, 42]
+  const test1 = new Tree(testArray)
+  // prettyPrint(test1.getRoot())
+  // console.log(test1.find(24))
+})
+
+test("levelOrder console logs the nodes in order", () => {
+  const testArray1 = [50, 24, 3001, 62, 99, 1, 42]
+  const test2 = new Tree(testArray1)
+  // test2.levelOrder((element) => console.log(element))
+  // console.log(test2.levelOrder())
+})
+
+test("postOrder console logs the nodes in order", () => {
+  const testArray = [50, 24, 3001, 62, 99, 1, 42]
+  const test = new Tree(testArray)
+  // console.log(test.postOrder())
+  // test.postOrder((element) => console.log(element))
+  // console.log(test.postOrder())
+})
+
+test("depth returns number representing depth of node", () => {
+  const testArray = [50, 24, 3001, 62, 99, 1, 42]
+  const test = new Tree(testArray)
+  // console.log(test.depth())
+})
+
+test("height returns number representing height of node", () => {
+  const testArray = [50, 24, 3001, 62, 99, 1, 42]
+  const test = new Tree(testArray)
+  // console.log(test.height())
+  test.insert(30000)
+  expect(test.height()).toBe(3)
+})
+
+// test("isBalance correctly determines if a tree is balanced", () => {
+//   const testArray = [50, 24, 3001, 62, 99, 1, 42]
+//   const test = new Tree(testArray)
+//   // prettyPrint(test.getRoot())
+//   expect(test.isBalanced()).toBe(true)
+//   test.insert(500)
+//   test.insert(7000)
+//   test.insert(60000)
+//   test.insert(532)
+//   expect(test.isBalanced()).toBe(false)
+// })
+
+test("rebalance correctly rebalances the tree", () => {
+  const testArray = [300, 420, 1, 3, 6000, 27, 843, 1932, 21562, 72]
+  const test = new Tree(testArray)
+  // prettyPrint(test.getRoot())
+  expect(test.isBalanced()).toBe(true)
+  test.insert(350)
+  test.insert(247)
+  test.insert(4000)
+  test.insert(5533)
+  test.insert(120)
+  test.insert(117)
   prettyPrint(test.getRoot())
-  test.insert(12)
-  prettyPrint(test.getRoot())
+  expect(test.isBalanced()).toBe(false)
+  test.rebalance()
+  expect(test.isBalanced()).toBe(true)
+  console.log(
+    "post: ",
+    test.postOrder(),
+    "in: ",
+    test.inOrder(),
+    "pre: ",
+    test.preOrder(),
+    "level: ",
+    test.levelOrder()
+  )
 })
